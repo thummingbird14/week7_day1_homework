97d43dd20adcf983f3188d59998e5bf3f1712c03
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-04-04 14:26:58 +0100

    commit MVP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [todos, setToDos] = useState([
+    "Buy Shopping",
+    "Clean Bathroom",
+    "Car's MOT"
+  ]);
+
+  const toDoNodes = todos.map((item, index) => {
+    return(
+      <li key={index}> 
+        <span>{item}</span>
+      </li>
+    )
+    });
+
+  const [newItem, setNewItem] = useState(' ');
+  
+  const handleItemInput = (event) => {
+    setNewItem(event.target.value);
+  }
+
+  const saveNewItem = (event) => {
+    event.preventDefault();
+    const copyToDos = [...todos];
+    console.log(newItem);
+    copyToDos.push(newItem);
+    setToDos(copyToDos);
+    setNewItem('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>ToDo List</h1>
+      <hr></hr>
+      <form onSubmit={saveNewItem}> 
+        <label htmlFor='new-item'>Add a new list item:</label>
+        <input id='new-item' type='text' value={newItem} onChange={handleItemInput}/>
+        <input type='submit' value='Save New Item'/>
+
+      </form>
+
+      <ul>
+        {toDoNodes}
+      </ul>
     </div>
   );
 }
